a25fa13188fedda3530ddc9bccdfed65cfa30c06
Iran Corrales <iran.corrales@wolterskluwer.com>
2019-12-12 18:52:43 -0700

    change table for divs

--- editTableCell.js
+++ editTableCell.js
@@ -1,71 +1,139 @@
-function editTableCell(table) {
-    var datas = table.querySelectorAll("td");
-    const possiblePadding = 7;
-    datas.forEach(function(tdElement){
+function editDivTable(table) {
+    let edit = false;
+
+    var datas = table.getElementsByClassName("mt-rc");
+    document.onkeydown = (e) => selectNext(e);
+    for(let i = 0; i < datas.length; i++) {
+        let tdElement = datas[i];
         tdElement.addEventListener("click", () => editContent(tdElement));
         tdElement.addEventListener("keydown", (e) => selectNext(e, tdElement));
-    });
+    };
 
-    var selectNext = function(e, tdElement) {
-        if (e.keyCode == 9) {
+    var selectNext = function(e) {
+        var tdElement = document.getElementsByClassName("selected")[0];
+        if (e.keyCode === 9) {
             e.preventDefault();
             if (tdElement.nextElementSibling != null) {
-                editContent(tdElement.nextElementSibling);
+                setSelectHighlight(tdElement.nextElementSibling);
             } else if (tdElement.parentNode.nextElementSibling != null) {
-                editContent(tdElement.parentNode.nextElementSibling.children[0]);
+                setSelectHighlight(tdElement.parentNode.nextElementSibling.children[0]);
             }
+            return;
+        }
+        
+        if (e.keyCode === 13) {
+            let input = document.getElementById("mt-input-edit");
+            if (input != undefined) {
+                input.blur();
+            }
+            return;
         }
 
-        if (e.keyCode == 39) {
+        if (e.keyCode === 27) {
+            edit = false;
+            let input = document.getElementById("mt-input-edit");
+            if (input != undefined) {
+                input.blur();
+            }
+            return;
+        }
+
+        if (edit) {
+            return;
+        }
+
+        if (e.keyCode === 39) {
             e.preventDefault();
             if (tdElement.nextElementSibling != null) {
-                editContent(tdElement.nextElementSibling);
+                setSelectHighlight(tdElement.nextElementSibling);
             }
+            return;
         }
 
-        if (e.keyCode == 37) {
+        if (e.keyCode === 37) {
             e.preventDefault();
             if (tdElement.previousElementSibling != null) {
-                editContent(tdElement.previousElementSibling);
+                setSelectHighlight(tdElement.previousElementSibling);
             }
+            return;
         }
-        if (e.keyCode == 40) {
+
+        if (e.keyCode === 40) {
             e.preventDefault();
             let i = 0;
             let child = tdElement;
             while( (child = child.previousElementSibling) != null ) i++;
             if (tdElement.parentNode.nextElementSibling != null) {
-                editContent(tdElement.parentNode.nextElementSibling.children[i]);
+                setSelectHighlight(tdElement.parentNode.nextElementSibling.children[i]);
             }
+            return;
         }
-        if (e.keyCode == 38) {
+
+        if (e.keyCode === 38) {
             e.preventDefault();
             let i = 0;
             let child = tdElement;
             while( (child = child.previousElementSibling) != null ) i++;
             if (tdElement.parentNode.previousElementSibling != null) {
-                editContent(tdElement.parentNode.previousElementSibling.children[i]);
+                setSelectHighlight(tdElement.parentNode.previousElementSibling.children[i]);
             }
+            return;
+        }
+
+        let regex = /[a-zA-Z0-9_.-]/;
+        console.log(e);
+        if (tdElement != undefined && e.key.match(regex) && e.key.length == 1){
+            editContent(tdElement, e.key);
         }
     }
 
-    var editContent = function(tdElement){
-        if (tdElement.getAttribute("edit") != "edit") {
-            tdElement.setAttribute("edit", "edit");
-            var input = document.createElement("INPUT");
-            input.setAttribute("type", "text");
-            input.value = tdElement.innerText;
-            tdElement.innerHTML = "";
-            tdElement.appendChild(input);
-            tdElement.classList.toggle("selected");
-            input.focus();
-            input.style.width = (tdElement.clientWidth - possiblePadding) + "px";
-            input.style.height = (tdElement.clientHeight - possiblePadding ) + "px";
-            input.addEventListener("blur", function(){
-                tdElement.innerHTML = input.value;
-                tdElement.setAttribute("edit", "");
-                tdElement.classList.toggle("selected");
-            });
+    var editContent = function(element, value){
+        if (!edit) {
+            setSelectHighlight(element);
+            setInputEdit(element, value);
         }
+    }
+
+    function setInputEdit(element, value){
+        value = value || ""; 
+        edit = true;
+        let domRect = element.getBoundingClientRect();
+        let input = document.createElement("INPUT");
+        input.setAttribute("type", "text");
+        input.id = "mt-input-edit";
+        input.style.top = domRect.top  + 1 + "px";
+        input.style.left = domRect.left + 1 + "px";
+        input.style.width = domRect.width - 1 + "px";
+        input.style.height = domRect.height - 3 + "px";
+        if (value === "" ) {
+            input.value = element.innerText ;
+        }
+        document.body.appendChild(input);
+        input.focus();
+        input.addEventListener("blur", function(){
+            if (edit) {
+                element.innerHTML = input.value;
+            }
+            input.parentNode.removeChild(input);
+            edit = false;
+        });
+    }
+
+    function setSelectHighlight(element) {
+        let oldElement = document.getElementsByClassName("selected")[0];
+        if (oldElement != undefined) {
+            oldElement.classList.toggle("selected");
+        }
+        let selector = document.getElementById("mt-selection-highlight");
+        let domRect = element.getBoundingClientRect();
+        let input = document.getElementById("mt-input-edit");
+        if (input != undefined) {
+            input.blur();
+        }
+        element.classList.toggle("selected");
+        selector.style.top = domRect.top - 1 + "px";
+        selector.style.left = domRect.left - 1 + "px";
+        selector.style.width = domRect.width + "px";
+        selector.style.height = domRect.height + "px";
     }
 }
